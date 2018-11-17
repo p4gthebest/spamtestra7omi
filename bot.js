7b21d81e0b8ff93472779feedb0345224682e427
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-11-17 13:56:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,5 +9,18 @@ channel.send(`**Road To 100 Credit**`);
 }, 25)
 })
 
+client.on("ready", () => {
+let channel =     client.channels.get("513336452898095119")
+setInterval(function() {
+channel.send('#daily');
+}, 25)
+})
+
+client.on("ready", () => {
+let channel =     client.channels.get("513336452898095119")
+setInterval(function() {
+channel.send('#credit');
+}, 25)
+})
 
 client.login(process.env.BOT_TOKEN);
