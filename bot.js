a9f3072c14935e26ceaffba3c16bd42c073abac7
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-11-17 12:19:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,103 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+console.log("Scrpit By Dream");
 
 
-client.on('message', message => {
-    if(message.content === 'حيدر'){
-        message.channel.send('يوسف')
-    }
+client.on("ready", () => {
+let channel =     client.channels.get("509316968193458176")
+setInterval(function() {
+channel.send(`**Road To 100 Credit**`);
+}, 25)
 });
-
-client.on('message', message => {
-    if(message.content === 'يوسف'){
-        message.channel.send('الو')
-    }
-});
-
-
-
-client.on('message', message => {
-    if(message.content === 'الو'){
-        message.channel.send('هلا')
-    }
-});
-
-
-
-client.on('message', message => {
-    if(message.content === 'هلا'){
-        message.channel.send('كيفك')
-    }
-});
-
-
-
-
-client.on('message', message => {
-    if(message.content === 'كيفك'){
-        message.channel.send('تمام')
-    }
-});
-
-
-
-
-
-
-client.on('message', message => {
-    if(message.content === 'حيدر'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'يوسف'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'الو'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'هلا'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'كيفك'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'تمام'){
-        message.channel.send('#daily')
-    }
-});
-
-
-
-
-
-
-client.on('message', message => {
-    if(message.content === '#daily'){
-        message.channel.send('#credit')
-    }
-});
-
-
-client.on('message', message => {
-    if(message.content === '#credit'){
-        message.channel.send('حيدر')
-    }
-});
-
 
 
 client.login(process.env.BOT_TOKEN);
