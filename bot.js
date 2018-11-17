054b9fe05832e76f8a1266b2124361f7f626ae49
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-11-17 13:43:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,25 +2,25 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-client.on("ready", () => {
-console.log(`Bot by P4G`); 
-client.user.setActivity('spaming in servers');
-let channel =     client.channels.get("513309819612954624")
-setInterval(function() {
-channel.send(`**Road To 100 Credit**`);
-})
-});
-
 client.on('message', message => {
-    if(message.content === 'check the credit'){
+    if(message.content === 'check'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-     if(message.content === 'check the credit'){
+    if(message.content === '#daily'){
         message.channel.send('#credit')
     }
+});
+
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("513309819612954624")
+setInterval(function() {
+channel.send(`**Road To 100 Credit**`);
+})
 });
 
 
