9d71405cc9b69473ef19197a5997f3eca30c30a8
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-11-17 14:07:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,17 +4,11 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("509316968193458176")
+let channel =     client.channels.get("513309819612954624")
 setInterval(function() {
-channel.send(`**Road To 100k Credit**`);
+channel.send(`**Road To 100 Credit**`);
 }, 25)
 })
 
-client.on("ready", () => {
-let channel =     client.channels.get("513336452898095119")
-setInterval(function() {
-channel.send('#daily');
-}, 25)
-})
 
 client.login(process.env.BOT_TOKEN);
