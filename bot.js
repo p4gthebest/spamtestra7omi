219786460f31c240c8f7f792ca82f7af4bb0baf3
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-11-17 13:21:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
+console.log(`Bot by P4G`); 
+client.user.setActivity('spaming in servers');
 let channel =     client.channels.get("513309819612954624")
 setInterval(function() {
 channel.send(`**Road To 100 Credit**`);
