1e2d2565238628622f5c67ee39e4e88868f87c73
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-11-23 11:07:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("P4G the best");
-
+var prefix = "0";
 
 client.on("ready", () => {
 let channel =     client.channels.get("513309819612954624")
 setInterval(function() {
 channel.send(`**Road To 100k Credit**`);
-}, 25)
+}, 100)
 })
+
+
+
+
 
 client.on('message', message => {
     if(message.content === 'check'){
@@ -23,6 +27,21 @@ client.on('message', message => {
 });
 
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 
 client.login(process.env.BOT_TOKEN);
